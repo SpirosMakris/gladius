1a92da76de90015b1e3bde1660447fa67510aae2
Spiros Makris <makris.spiros@gmail.com>
2020-08-06 13:51:13 +0300

    Created SDL window

---
+++ doc/TODO.md
@@ -0,0 +1,5 @@
+## Platform
+* For now we are using SDL2 as a base platform layer (Debug DLL 2.0.12).
+    Maybe do:
+        * static-link feature
+        * Use release version for release build
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting up..");
+
+    let sdl = sdl2::init()
+                .expect("Failed to init SDL");
+    
+    let video_subsystem = sdl.video()
+                .expect("Failed to init Video subsystem");
+    
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .expect("Failed to create window");
+    
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(17));
+    }
+
 }
